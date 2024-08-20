5fcaa3109eb333c784ab4a195027331b4481ef70
david <david.zhang@ewov.com.au>
2024-08-21 00:36:34 +1000

    Add null handling notes

--- models/stage/stg_customers.sql
+++ models/stage/stg_customers.sql
@@ -7,7 +7,7 @@ renamed as (
         "CustomerID"::int as customer_id,
         coalesce("Name", 'Unknown')::varchar as customer_name,
         "Age"::int as customer_age,
-        coalesce("Gender", 'Unknown')::varchar as customer_gender,
+        coalesce("Gender", 'Unknown')::varchar as customer_gender, -- example null handling, might be more prudent to try to understand cause of null and handle them downstream
         coalesce("City", 'Unknown')::varchar as customer_city
     from source
 )
--- models/stage/stg_products.sql
+++ models/stage/stg_products.sql
@@ -6,7 +6,7 @@ renamed as (
     select
         "ProductID"::int as product_id,
         "CategoryID"::int as category_id,
-        coalesce("ProductName", 'Unknown')::varchar as product_name,
+        "ProductName"::varchar as product_name,
         "Price"::numeric(16,2) as product_price_dollars
     from source
 )
